d11979f97e81f7ad9e7d33437549121230097b80
DarkLordGravigas <lance_hycross@yahoo.ca>
2015-08-11 10:53:08 -0400

    New adjustments
    Setting up the text file correspondances

---
+++ Game/Game/Music/MusicEditor/Music_Editor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Music {
+    class Music_Editor {
+        /* ok this is to create an easier music design element. concept is going to be simple
+         * we are goint to have a sound library for a base resource to make music with.
+         * using mml is going to make music fun for more advanced people. but for people who
+         * arent so talented this is going to be in game music editor. im going to work on the
+         * item we will call "The sound station" OST are going to be our game specific
+         * bgms stored there, and  UBST for "user based sound track".
+         * \
+    }
+}
